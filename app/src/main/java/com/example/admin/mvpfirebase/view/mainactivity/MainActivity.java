c0f54fffc5ea01b99d0c04a94e656ee05883a3d6
RockDrigo10 <rodrigo.chavez10@gmail.com>
2017-08-23 16:39:12 -0400

    Adding object into Firebase

--- app/src/main/java/com/example/admin/mvpfirebase/view/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/admin/mvpfirebase/view/mainactivity/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.TextView;
 
 import com.example.admin.mvpfirebase.Injection.mainactivity.DaggerMainActivityComponent;
 import com.example.admin.mvpfirebase.R;
+import com.example.admin.mvpfirebase.model.Movie;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -16,12 +19,14 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
     TextView tvText;
     @Inject
     MainActivityPresenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvText = (TextView) findViewById(R.id.tvText);
         setupDaggerComponent();
+        presenter.init();
         presenter.attachView(this);
         presenter.saveDataToCloud("John Snow");
     }
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
     public void getDataFirebase(View view) {
         presenter.getDataToCloud();
     }
+
     private void setupDaggerComponent() {
         DaggerMainActivityComponent.create().inject(this);
     }
@@ -48,4 +54,22 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
         tvText.setText(s);
     }
 
+    @Override
+    public void updateMovieList(List<Movie> movieList) {
+        for(Movie movie: movieList){
+            Log.d(TAG, "updateMovieList: "+ movie.toString());
+        }
+    }
+
+    public void updateFirebaseDb(View view) {
+        switch (view.getId()) {
+            case R.id.btnPushMovie:
+                Movie movie = new Movie("Avengers", "sdadss", 2015);
+                presenter.pushToDb(movie);
+                break;
+            case R.id.btnGetMovie:
+                presenter.getMovies();
+                break;
+        }
+    }
 }
